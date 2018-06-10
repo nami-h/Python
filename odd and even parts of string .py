ab47e2aaa30a4afa05683c307f842fb448fb61d6
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-10 01:12:36 -0400

    Create odd and even parts of string .py

---
+++ odd and even parts of string .py
@@ -0,0 +1,9 @@
+word=input("a word/string: ")
+odd=[]
+even=[]
+for each in range(0,len(word),2):
+    odd.append(word[each])
+for every in range(1,len(word),2):
+    even.append(word[every])
+print("odd list: ", odd, " & even list: ", even)
+    
