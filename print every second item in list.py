a4f2f24a59b8634e787aa1c1d79e460d4eaca59a
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-10 01:03:57 -0400

    Create print every second item in list.py

---
+++ print every second item in list.py
@@ -0,0 +1,6 @@
+spell_list = ["Tuesday", "Wednesday", "February", "November", "Annual", "Calendar", "Solstice"]
+print("printing from first with a gap of one")
+for ran in range(0,len(spell_list),2):
+    print(spell_list[ran])
+    
+    
