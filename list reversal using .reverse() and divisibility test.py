9de5ac1132215e4414bea35bb579fea4d19491c9
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-12 22:44:08 -0400

    Create list reversal using .reverse() and divisibility test.py

---
+++ list reversal using .reverse() and divisibility test.py
@@ -0,0 +1,12 @@
+cities_1 = ["Dubai", "Mexico City", "São Paulo", "Hyderabad"]
+cities_1.reverse()
+print(cities_1)
+num=[1,2,3,4,5]
+num.reverse()
+print(num)
+print(len(num))
+for n in num:
+    if (n/3==int(n/3)):
+        print(n)
+    else:    
+        pass
