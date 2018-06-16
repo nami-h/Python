da9ccd32c21ac496070ac81600179751923acbb3
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-16 15:31:09 -0400

    Create ending each line with end=.py

---
+++ ending each line with end=.py
@@ -0,0 +1,27 @@
+hello=('hello')
+print('~'.join(list(hello)))
+
+print('Hello',end='_')
+print('world')
+
+print('hello world',end='\n')
+print('still something to learn about python')
+
+for let in "concatenation":
+    print(let,end='#')
+    
+print('\n')    
+    
+for lin in "The String":
+    print(lin,end='- ')
+    
+print('\n')    
+    
+print('The',end='- ')    
+print('String',end='- ')
+print(' ',end='- ')
+
+Msg_characters = list("Always test your code")
+fact=Msg_characters+list('The sun and the earth are spheres too, right?')
+for i in fact:
+    print((i), end='\n')
