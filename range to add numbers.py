d28e990d6851eb06a04c022c28ef6c1cfc7c63d1
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-16 17:34:20 -0400

    Create range to add numbers.py

---
+++ range to add numbers.py
@@ -0,0 +1,14 @@
+twenties=[]
+for i in range(20,30):
+    twenties.append(i)
+print(twenties) 
+
+total=0
+for num in twenties: 
+    total+=num
+print(total)   
+
+su=0
+for n in range(20,30):
+    su+=n
+print(su) 
