6c37d1e250b61d2e90f45e68f7ac3b055acd5fda
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-16 14:47:47 -0400

    Create .join() with separator.py

---
+++ .join() with separator.py
@@ -0,0 +1,19 @@
+tip_words = ['Notebooks', 'can', 'be', 'exported', 'as', '.pdf'] 
+tip=" ".join(tip_words)
+print(tip)
+
+python=['p','y','t','h','o','n']
+print("".join(python))
+
+word='Iteration'
+print('-'.join(word))
+print(" ".join(word))
+print('...'.join(word))
+
+letters = ["A", "s", "t", "e", "r", "i", "s", "k"]
+print('*'.join(letters))
+
+sep=input("Print a separator: A-Z,a-z,0-9,!@#$%^&*()-_+=:'?><,.|\/")
+phrase_words = ['Jack', 'and', 'Jill', 'went', 'up', 'the', 'hill', 'To', 'fetch', 'a', 'pail', 'of', 'water']
+print(phrase_words)
+print("with separator: ", sep.join(phrase_words))
