4aba3f06bb33ffb4ad66d72450c6be29df344e3f
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-16 14:04:48 -0400

    Create split string into list and first and every alternate word.py

---
+++ split string into list and first and every alternate word.py
@@ -0,0 +1,10 @@
+rhyme = 'Jack and Jill went up the hill To fetch a pail of water' 
+split=rhyme.split()
+print(split)
+
+for word in split:
+    print(word)
+    
+code_tip = "Python uses spaces for indentation"
+sp=code_tip.split()
+print(sp[::2])
