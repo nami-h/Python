cc427814ff1b4d0fa720a2386cd10c9ac1cde9b1
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-16 22:29:08 -0400

    Update index finding even and odd elements in list.py

--- index finding even and odd elements in list.py
+++ index finding even and odd elements in list.py
@@ -2,7 +2,6 @@ elements = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon', 'Ni
  'Neon', 'Sodium', 'Magnesium', 'Aluminum', 'Silicon', 'Phosphorus', 'Sulfur', 'Chlorine', 'Argon', 
  'Potassium', 'Calcium']
 
-length=len(elements)
 for ele in elements:
     if ((elements.index(ele))%2!=0):
         print(ele, "At. no. : ", elements.index(ele)+1)
