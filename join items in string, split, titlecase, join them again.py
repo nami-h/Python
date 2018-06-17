f7dea08431485bc5d54c4e170186f95dc719f469
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-16 23:35:43 -0400

    Create join items in string, split, titlecase, join them again.py

---
+++ join items in string, split, titlecase, join them again.py
@@ -0,0 +1,13 @@
+halogens = ['Chlorine', 'Florine', 'Bromine', 'Iodine']
+print(', '.join(halogens))
+
+code_tip ="Read code aloud or explain the code step by step to a peer"
+x=code_tip.split()
+print(x)
+
+y=code_tip.title()
+print(y)
+
+print("".join(x))
+
+print("".join(y.split()))
