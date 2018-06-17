f33d4e18fb231342461812fd3627a8af2066609e
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-17 00:10:28 -0400

    Create join and capitalize each word.py

---
+++ join and capitalize each word.py
@@ -0,0 +1,14 @@
+foot_bones = ["calcaneus", "talus", "cuboid", "navicular", "lateral cuneiform", 
+            "intermediate cuneiform", "medial cuneiform"]
+
+l=[]
+for each in foot_bones:
+    l.append(each.title())
+print(l) 
+
+j=[]
+j.append(" ".join(l))
+print(j)  
+
+
+
