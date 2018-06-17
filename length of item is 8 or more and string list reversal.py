4ea748e2885bf11fe2db0328f762c9a37a95bf5c
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-16 22:48:34 -0400

    Create length of item is 8 or more and string list reversal.py

---
+++ length of item is 8 or more and string list reversal.py
@@ -0,0 +1,5 @@
+spell_list = ["Tuesday", "Wednesday", "February", "November", "Annual", "Calendar", "Solstice"]
+spell_list.reverse()
+for item in spell_list:
+    if (len(item) >=8):
+        print(item)
