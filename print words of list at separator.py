80b5ccbe07b018ce5c1a7f7f0d6a8890a1b4aa52
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-16 23:03:41 -0400

    Create print words of list at separator.py

---
+++ print words of list at separator.py
@@ -0,0 +1,8 @@
+code_tip ="Read code aloud or explain the code step by step to a peer"
+x=code_tip.split('o')
+print(x)
+
+poem = "The bright brain, has bran!"
+l=poem.split('b')
+for part in l:
+    print(part)
