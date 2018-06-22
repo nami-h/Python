5e898494d46eb23e82d8789cc6e37d11fe4be903
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-22 12:05:21 -0400

    Update file import .readline() while loop.py
    output of the first one is: 
    
    LOOPS I REPEAT
    LOOPS
    LOOPS
    LOOPS
    I REPEAT
    UNTIL I
    BREAK
    
    
    output of the second one is: 
    
    RED
    
    ORANGE
    
    YELLOW
    
    GREEN
    
    BLUE
    
    INDIGO
    
    VIOLET

--- file import .readline() while loop.py
+++ file import .readline() while loop.py
@@ -1,3 +1,4 @@
+#poem file: 
 !curl https://raw.githubusercontent.com/MicrosoftLearning/intropython/master/poem1.txt -o poem1.txt 
 poem1=open('poem1.txt','r')    
 poem_line=poem1.readline()
@@ -5,3 +6,13 @@ while poem_line:
     print(poem_line[:-1].upper())
     poem_line=poem1.readline()
 poem1.close()  
+
+#rainbow colors file:
+!curl  https://raw.githubusercontent.com/MicrosoftLearning/intropython/master/rainbow  -o rainbow.txt
+rainbow=open('rainbow.txt','r')
+rainbow_line=rainbow.readline()
+while rainbow_line:
+    print(rainbow_line.upper())
+    rainbow_line=rainbow.readline()
+rainbow.close
+
