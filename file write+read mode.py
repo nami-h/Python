0e780e81b5cb3f640366d5c3086cf0da2ad7a043
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-25 15:09:21 -0400

    Create file write+read mode.py

---
+++ file write+read mode.py
@@ -0,0 +1,3 @@
+'''after any write, the pointer is at the end of text that has been written
+to read the entire file, the pointer needs to be at the beginning of the file - see .seek() below to set the file pointer'''
+
