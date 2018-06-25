c3994e5d83dbca0e1c5fbe701b6454c18a38c18b
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-25 15:31:26 -0400

    Update file write+read mode.py
    Poems about soccer, 
    aka football, 
    aka the beautiful game, 
    in time for the World Cup.

--- file write+read mode.py
+++ file write+read mode.py
@@ -1,3 +1,11 @@
 '''after any write, the pointer is at the end of text that has been written
 to read the entire file, the pointer needs to be at the beginning of the file - see .seek() below to set the file pointer'''
 
+new_file=open('new_file.txt', 'w+')
+new_file.write("Poems about soccer, \naka football, \naka the beautiful game, \nin time for the World Cup.")
+new_file.seek(0)
+next=new_file.read()
+while(next):
+    print(next)
+    next=new_file.read()
+new_file.close()  
