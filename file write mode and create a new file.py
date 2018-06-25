b53f2453a0194a2ec7bfdc13109df6faca459701
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-25 14:56:12 -0400

    Update file write mode and create a new file.py
    Dayum.....! The power!!!
    
    Weekdays assemble

--- file write mode and create a new file.py
+++ file write mode and create a new file.py
@@ -2,7 +2,7 @@
 new_file=open('poem2.txt','w')
 print(new_file)
 new_file.write('Dayum.....! The power!!!')
-new_file.write("\n \tWeekdays assemble ")
+new_file.write("\nWeekdays assemble ")
 new_file.close()
 new_file=open('poem2.txt','r')
 lines=new_file.readline()
