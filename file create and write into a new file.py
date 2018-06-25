e4fca60962a8cb40320a2bf3b9078458aaec7e81
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-25 14:53:00 -0400

    Create file create and write into a new file.py
    Dayum.....! The power!!!
    
    Weekdays assemble

---
+++ file create and write into a new file.py
@@ -0,0 +1,12 @@
+
+new_file=open('poem2.txt','w')
+print(new_file)
+new_file.write('Dayum.....! The power!!!')
+new_file.write("\n \tWeekdays assemble ")
+new_file.close()
+new_file=open('poem2.txt','r')
+lines=new_file.readline()
+while lines:
+    print(lines)
+    lines=new_file.readline()
+new_file.close()
