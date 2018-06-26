e8e757efeb86bba7f1b357790bebdf47561b49a8
nami-h <35375225+nami-h@users.noreply.github.com>
2018-06-25 20:43:42 -0400

    Create file whence current location .seek().py
    first second third fourth--Middle sentence--

---
+++ file whence current location .seek().py
@@ -0,0 +1,7 @@
+tips_file=open('code_tips.txt','w+')
+tips_file.write("first second third fourth")
+tips_file.seek(0,1) # current location
+tips_file.write("--Middle sentence--")
+tips_file.seek(0)
+tips_text=tips_file.read()
+print(tips_text)
