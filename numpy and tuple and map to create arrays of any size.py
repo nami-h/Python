cdf45089713d99bfbfdbcfd551e56c2f52c9d995
nami-h <35375225+nami-h@users.noreply.github.com>
2018-10-08 20:38:08 -0400

    Create numpy and tuple and map to create arrays of any size.py

---
+++ numpy and tuple and map to create arrays of any size.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+num=tuple(map(int,input().split()))
+
+a=np.zeros(num, dtype='int')
+print(a)
+b=np.ones(num, dtype='int')
+print(b)
+
+
+"""[[[0 0 0]
+  [0 0 0]
+  [0 0 0]]
+
+ [[0 0 0]
+  [0 0 0]
+  [0 0 0]]
+
+ [[0 0 0]
+  [0 0 0]
+  [0 0 0]]]
+[[[1 1 1]
+  [1 1 1]
+  [1 1 1]]
+
+ [[1 1 1]
+  [1 1 1]
+  [1 1 1]]
+
+ [[1 1 1]
+  [1 1 1]
+  [1 1 1]]]
+  """
