d8a99d17e45ff02d2f6f50822cd9d7a2ffb075f7
nami-h <35375225+nami-h@users.noreply.github.com>
2018-11-19 23:39:08 -0500

    Create command line arguments basics.py

---
+++ command line arguments basics.py
@@ -0,0 +1,15 @@
+import sys
+lst=sys.argv
+for i in lst: 
+    print(i)
+print(len(lst))
+print(lst[0])
+
+
+'''
+/srv/conda/lib/python3.6/site-packages/ipykernel_launcher.py
+-f
+/home/jovyan/.local/share/jupyter/runtime/kernel-dad2b9d8-f54b-44cf-9fe1-92e1f339349b.json
+3
+/srv/conda/lib/python3.6/site-packages/ipykernel_launcher.py
+'''
