0799aba8dba0924a038563f53cd21f2fd99e10b5
nami-h <35375225+nami-h@users.noreply.github.com>
2018-11-19 22:34:39 -0500

    Create split a line of numbers with spaces into numbers.py

---
+++ split a line of numbers with spaces into numbers.py
@@ -0,0 +1,18 @@
+lst=[x for x in input("Enter three numbers").split()]
+print(lst)
+
+lst=[int(x) for x in input("Enter three numbers").split(',')]
+print(lst)
+
+
+
+
+'''
+Enter three numbers9 2 -3
+['9', '2', '-3']
+
+Enter three numbers2,5,98
+[2, 5, 98]
+
+
+'''
