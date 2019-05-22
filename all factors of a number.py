cb48c03f9e7b9bd7742982c61f096ce6af1dd949
nami-h <35375225+nami-h@users.noreply.github.com>
2019-05-21 21:53:18 -0400

    Update all factors of a number.py

--- all factors of a number.py
+++ all factors of a number.py
@@ -3,5 +3,32 @@ l=[]
 for i in range(1, n):
     if n%i==0:
         l.append(i)
+print(l)                           
+
+
+
+#better time complexity
+
+n=int(input())
+l=[]
+for i in range(2, round(n/2)+1):
+    if n%i==0:
+        l.append(i)
     
 print(l)
+
+
+
+#better time complexity O(sqrt(n))
+
+import math
+
+n=int(input())
+l=[]
+for i in range(1, round(math.sqrt(n))+1):
+    if n%i==0:
+        l.append(i)
+        if i!=int(math.sqrt(n)):
+            l.append(int(n/i))
+    
+print(sorted(l))
