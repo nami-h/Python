8ce9fefdc75f5a4f3b25f6d2c8686ea0f3961bbe
nami-h <35375225+nami-h@users.noreply.github.com>
2019-05-21 23:37:02 -0400

    Update prime or not.py

--- prime or not.py
+++ prime or not.py
@@ -2,10 +2,10 @@ from math import sqrt
 class Solution:
     def isPrime(self, A):
         if A==1:
-            return 0                   #prime
+            return 0                   #not prime
         elif A==2:
-            return 1                   #not prime
+            return 1                   #prime
         for i in range(2, int(sqrt(A))+1):
             if (A%i==0):
-                return 0              #prime
-        return 1                       #not prime
+                return 0              #not prime
+        return 1                       #prime
