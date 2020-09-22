63c139eaee8ff13dbc707a28495a54aa34412dd0
nami-h <35375225+nami-h@users.noreply.github.com>
2020-09-22 15:56:40 -0400

    Update practice dict.py

--- practice dict.py
+++ practice dict.py
@@ -14,3 +14,32 @@ print(net_device2)
 net_device3=net_device   #another dict copy 
 print(net_device3 is net_device2)  #check true or false
 print(net_device3==net_device) #check true or false
+
+
+print(net_device.get('model'))    #non existent key
+print(net_device.get('vendor'))   #getting values corresponding to keys
+print(net_device.get('model',999)) #initializing new key value pair
+
+print(dir(net_device2))  
+net_device4=net_device.copy() #making copy 
+print(net_device4)
+print(net_device4 is net_device) #not identical
+net_device['model']=998
+print(net_device)
+print(net_device.pop('model')) #popping values
+print(net_device)
+net_device2={'model':551} #change entire dict
+print(net_device2)
+net_device.update(net_device2) #update 
+print(net_device)
+
+for key in net_device:  #keys
+    print(key)
+for v in net_device.values():  #values
+    print(v)
+for v in net_device.items():   #key value pairs
+    print(v)
+for k,v in net_device.items(): #storing k and v in tuple
+    print(k)
+    print(v)
+    print('-'*30)
